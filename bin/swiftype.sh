1d40c3e5582c6da1875dc3d21b8e201684f1609d
Joe Siewert <jsiewert@cloudbees.com>
2020-10-04 19:40:09 -0500

    Fix Swiftype recrawl

--- bin/swiftype.sh
+++ bin/swiftype.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# trigger a Swiftype recrawl of the documentation pages
+# Trigger a Swiftype recrawl of the documentation pages
 log() { echo -e "\033[36m$@\033[39m"; }
-log "Trigger Swiftype recrawl."
-curl -XPUT -H 'Content-Length: 0' "https://api.swiftype.com/api/v1/engines/codeship-docs/domains/${SWIFTYPE_DOMAIN}/recrawl.json?auth_token=${SWIFTYPE_AUTH_TOKEN}"
+log "Trigger Swiftype recrawl"
+curl -X PUT -H 'Content-Length: 0' "https://api.swiftype.com/api/v1/engines/${SWIFTYPE_ENGINE}/domains/${SWIFTYPE_DOMAIN}/recrawl.json?auth_token=${SWIFTYPE_AUTH_TOKEN}"
